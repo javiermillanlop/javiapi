09f6201332c79b7afa1f75c3321da5e96133f8af
Javier Millan Lopez <javier.millan@businessmind.es>
2022-02-13 00:25:30 +0100

    noveno commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'POST':
+        print("Data received from Webhook is: ", request.json)
+        return "Webhook received!"
+    if request.method == 'GET':
+        return "service up!"
